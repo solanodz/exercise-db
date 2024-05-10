89f4a0277ebb0523660d83da1374f51d4ecda4e5
Solano de Zuasnabar <sdezuasnabar@gmail.com>
2024-05-10 10:38:47 -0300

    translating login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -24,17 +24,17 @@ export default function LoginPage() {
             <div>
                 <CardHeader className='border-b border-slate-300  flex flex-col gap-2'>
                     <div className=''>
-                        <CardTitle className='font-bold'>Ingresa como Cliente</CardTitle>
-                        <CardDescription>Ingresa a tu cuenta o crea una para acceder a los ejercicios</CardDescription>
+                        <CardTitle className='font-bold'>Login as Client</CardTitle>
+                        {/* <CardDescription>Ingresa a tu cuenta o crea una para acceder a los ejercicios</CardDescription> */}
                     </div>
                     <form>
-                        <Button formAction={loginWithGoogle} className='w-full'><Image src={'/google-logo.png'} height={320} width={400} quality={100} alt='google logo' className='w-10 h-fit' />  Ingresar con Google</Button>
+                        <Button formAction={loginWithGoogle} className='w-full'><Image src={'/google-logo.png'} height={320} width={400} quality={100} alt='google logo' className='w-10 h-fit' />  Login with Google</Button>
                     </form>
                 </CardHeader>
             </div>
             <CardFooter className='flex flex-col w-full gap-4 pt-4'>
-                <CardTitle className='font-bold text-left mr-auto'>Ingresa como Administrador</CardTitle>
-                <CardDescription>Si eres administrador, ingresa a tu cuenta para agregar ejercicios.</CardDescription>
+                <CardTitle className='font-bold text-left mr-auto'>Login as Administrator</CardTitle>
+                <CardDescription>If you are an administrator, log in to your account to add exercises.</CardDescription>
 
                 <form className='flex flex-col gap-3 w-full'>
                     <Label htmlFor="email">Email:</Label>
@@ -42,7 +42,7 @@ export default function LoginPage() {
                     <Label htmlFor="password">Password:</Label>
                     <Input id="password" name="password" type="password" required />
                     {/* <Button formAction={signup}>Sign up</Button> */}
-                    <Button formAction={login} className='w-full'>Ingresar</Button>
+                    <Button formAction={login} className='w-full'>Submit</Button>
 
                 </form>
             </CardFooter>
